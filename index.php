f21c23f9853666394085e1c794b34bf05c0f6cb9
local <zhao.terryy@gmail.com>
2016-05-16 18:29:49 -0400

    updates rev03

--- index.php
+++ index.php
@@ -17,17 +17,20 @@
 			$_SESSION["timeStamp"] = time();
 		}
 	?>
-	<div id="loginBox">
-		<form action="validate.php" method="post">
-			<input class="textboxes" id="idInput" type="password" name="key" placeholder="ID"></input>
-			<input class="textboxes" id="secretInput" type="password" name="secret" placeholder="Secret"></input>
-			<input id="submit" type="submit" value="Login"></input>	
-		</form>
-		<?php 
-			if ($_SESSION["hasAttempted"]) {
-				echo '<span id="error"> Invalid ID.</span>';
-			}
-		?>
+	<div id="loginWrapper">
+		<img id="imgCodex" src="codex.png">
+		<div id="loginBox">
+			<form action="validate.php" method="post">
+				<input class="textboxes" id="idInput" type="password" name="key" placeholder="ID"></input>
+				<input class="textboxes" id="secretInput" type="password" name="secret" placeholder="Secret"></input>
+				<input id="submit" type="submit" value="Login"></input>	
+			</form>
+			<?php 
+				if ($_SESSION["hasAttempted"]) {
+					echo '<span id="error"> Invalid ID.</span>';
+				}
+			?>
+		</div>
 	</div>
 	<script src="func.js"></script>
 </body></html>

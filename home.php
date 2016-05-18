dfe2129e9a6a57c3ea0c7254c5e66c1ad1cd3352
local <zhao.terryy@gmail.com>
2016-05-18 18:19:47 -0400

    update rev06 added home.php

--- home.php
+++ home.php
@@ -15,12 +15,12 @@
 	?>
 	<div id="topNav">
 		<nav id="navBar">
-			<a href="home.php">Home</a> |
-			<a href="home.php">Tasks</a> |
-			<a href="home.php">Schedule</a> |
-			<a href="https://drive.google.com/drive/folders/0B-9kDh5lDu5kQW9sekpBeUhQcVk" target="_blank">Google Drive</a> |
-			<a href="https://github.com/codexstudio" target="_blank">GitHub</a> |
-			<a href="logout.php">Logout</a>
+			<a class="navBar-links" href="home.php">Home</a> |
+			<a class="navBar-links" href="home.php">Tasks</a> |
+			<a class="navBar-links" href="home.php">Schedule</a> |
+			<a class="navBar-links" href="https://drive.google.com/drive/folders/0B-9kDh5lDu5kQW9sekpBeUhQcVk" target="_blank">Google Drive</a> |
+			<a class="navBar-links" href="https://github.com/codexstudio" target="_blank">GitHub</a> |
+			<a class="navBar-links" href="logout.php">Logout</a>
 		</nav>
 		<img id="logo" src="codex.png"></img>
 	</div>
